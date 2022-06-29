8f97156a4f5d6c3c825f966cbb170eb5e137cf21
Guillermo García López <memogl25@gmail.com>
2022-06-29 00:13:49 -0500

    Creando formularios anidados

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+    'Provider', new webpack.ProviderPlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+    })
+)
+
 module.exports = environment
